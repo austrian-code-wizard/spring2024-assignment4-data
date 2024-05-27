7d20fe23ebe22dbac4e41599032ab45c521e2ffe
Moritz Stephan <moritz.stephan@stanford.edu>
2024-05-27 14:00:29 -0700

    adding main script

--- cs336-data/cs336_data/run.py
+++ cs336-data/cs336_data/run.py
@@ -1,13 +1,14 @@
 from fastwarc import ArchiveIterator
 import argparse
 from tqdm import tqdm
-from cs336_data.utils import extract_text, identify_language, mask_emails, mask_phone_numbers, mask_ipv4
+from cs336_data.utils import extract_text, identify_language, mask_emails, mask_phone_numbers, mask_ipv4, classify_nsfw, classify_toxic_speech
 
 
 def main(
         extract: bool,
         identify: bool,
         mask_pii: bool,
+        classify: bool,
         input_path: str,
         output_path: str
 ):
@@ -16,7 +17,10 @@ def main(
             output = str(record.http_headers)
             content = record.reader.read()
             if identify:
-                output += str(identify_language(extract_text(content)))
+                output += f"\nLanguage: {identify_language(extract_text(content))}\n"
+            if classify:
+                output += f"\nNSFW: {classify_nsfw(content)}\n"
+                output += f"\nToxic: {classify_toxic_speech(content)}\n"
             if extract:
                 content = extract_text(content)
             if mask_pii:
@@ -33,7 +37,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--extract', action='store_true')
     parser.add_argument('--identify', action='store_true')
-    parser.add_argument('--mask-pii', action='store_true')
+    parser.add_argument('--mask', action='store_true')
+    parser.add_argument('--classify', action='store_true')
     parser.add_argument('input_path')
     parser.add_argument('output_path')
     args = parser.parse_args()
@@ -41,4 +46,4 @@ if __name__ == '__main__':
     if args.mask_pii and not args.extract:
         raise ValueError("Cannot mask PII without extracting text")
 
-    main(args.extract, args.identify, args.mask_pii, args.input_path, args.output_path)
+    main(args.extract, args.identify, args.mask, args.classify, args.input_path, args.output_path)
