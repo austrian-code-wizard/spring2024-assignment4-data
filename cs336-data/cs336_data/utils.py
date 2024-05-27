3b62569aff69a0178aeb3dac2f42977b7ea67003
Moritz Stephan <moritz.stephan@stanford.edu>
2024-05-27 12:14:08 -0700

    adding main script

--- cs336-data/cs336_data/utils.py
+++ cs336-data/cs336_data/utils.py
@@ -1,8 +1,22 @@
+import fasttext
 from resiliparse import parse
 from resiliparse.extract import html2text
+
+
+default_model = fasttext.load_model("/home/shared/lid.176.bin")
 
 
 def extract_text(inp: bytes) -> str:
     encoding = parse.encoding.detect_encoding(inp)
     decoded = inp.decode(encoding, errors="replace")
     return html2text.extract_plain_text(decoded)
+
+
+def identify_language(text: str, model: str | None = None) -> tuple[str, float]:
+    if model is None:
+        model = default_model
+    else:
+        model = fasttext.load_model(model)
+    results = model.predict(text)
+    results = [(l, v) for l, v in zip(*results)]
+    return sorted(results, key=lambda x: x[1], reverse=True)[0]
