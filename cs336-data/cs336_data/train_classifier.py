25d930687c7fb48700901a67999446b9e254b79e
Moritz Stephan <moritz.stephan@stanford.edu>
2024-05-27 14:58:54 -0700

    adding main script

--- cs336-data/cs336_data/train_classifier.py
+++ cs336-data/cs336_data/train_classifier.py
@@ -1,3 +1,4 @@
+import os
 import gzip
 import random
 random.seed(42)
@@ -5,6 +6,7 @@ random.seed(42)
 
 NUM_SAMPLES = 64000
 URL_PATH = "/home/shared/enwiki-20240420-extracted_urls.txt.gz"
+OUTPUT_PATH = "./data"
 
 
 def sample_urls_sharded(num_samples: int, num_shards: int = 64) -> list[str]:
@@ -13,9 +15,10 @@ def sample_urls_sharded(num_samples: int, num_shards: int = 64) -> list[str]:
     urls = random.sample(urls, num_samples)
     shards = [urls[i::num_shards] for i in range(num_shards)]
     for i, shard in shards:
-        with open(f"subsampled_positive_urls-{i}.txt", "w") as f:
+        with open(f"{OUTPUT_PATH}/subsampled_positive_urls-{i}.txt", "w") as f:
             f.writelines(shard)
 
 
 if __name__ == "__main__":
+    os.makedirs(OUTPUT_PATH, exist_ok=True)
     sample_urls_sharded(NUM_SAMPLES)
